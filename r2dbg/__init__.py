9e0a9ae716dcd48234515b50654a4d573bd3209b
Enigmatrix <enigmatrix2000@gmail.com>
2018-12-11 20:59:51 +0800

    interface with pwntools

---
+++ r2dbg/__init__.py
@@ -0,0 +1,9 @@
+from pwn import *
+
+def r2dbg(args, r2script=None, r2port=None, exe=None, ssh=None, env=None, sysroot=None, **kwargs):
+    gdbscript = ""
+    if r2port:
+        gdbscript += "#>port="+str(r2port)+"\n"
+    gdbscript += '\n'.join(['#r2.cmd("{}")'.format(c) for c in r2script.split('\n')])
+    return gdb.debug(args, gdbscript, exe, ssh, env, sysroot, **kwargs)
+
