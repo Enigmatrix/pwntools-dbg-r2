b3f01d1e57f2937c69e917f24dc4ed015d8dd93a
Enigmatrix <enigmatrix2000@gmail.com>
2018-12-11 17:59:13 +0800

    Setup file + mod

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name='pwntools-dbg-r2',  
+    version='0.1',
+    scripts=['pwntools-gdb'] ,
+    author="Enigmatrix",
+    author_email="enigmatrix2000@gmail.com",
+    description="Package to enable debugging with Radare2 in pwntools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Enigmatrix/pwntools-dbg-r2",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+ )
